d21eff63c5a4ba31f36a38415c9c0400a517ac90
KKKKKKKKKKKiss <kristinaivanikova@gmail.com>
2022-12-01 18:05:26 +0700

    fast working with a negative deg

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
-double my_pow(double num , unsigned int deg)
-{ 
-    double res =1;
-    for (int i=0; i<deg; i++)
-    {
-     res*=num;   
+double my_pow(long num, long deg) {
+    double result = 1;
+    if(deg < 0) {
+        deg = -deg;
+
+        while(deg) {
+            if (deg % 2 == 0) {
+                deg /= 2;
+                num *= num;
+            }
+            else {
+                deg--;
+                result *= num;
+            }
+        }
+
+        return 1 / result;
     }
-    return res;
-};
+    else {
+        while(deg) {
+            if (deg % 2 == 0) {
+                deg /= 2;
+                num *= num;
+            }
+            else {
+                deg--;
+                result *= num;
+            }
+        }
+
+        return result;
+    }
+}
